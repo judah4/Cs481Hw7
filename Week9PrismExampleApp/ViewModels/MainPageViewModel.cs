bbd2ebce6daad8f915aee617fa885ae2f159d136
Judah Perez <tythin4@gmail.com>
2017-11-09 20:02:30 -0800

    initial

---
+++ Week9PrismExampleApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,108 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using static Week9PrismExampleApp.Models.WeatherItemModel;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Week9PrismExampleUnitTests")]
+namespace Week9PrismExampleApp.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+		public DelegateCommand NavToNewPageCommand { get; set; }
+		public DelegateCommand GetWeatherForLocationCommand { get; set; }
+		public DelegateCommand<WeatherItem> NavToMoreInfoPageCommand { get; set; }
+
+		private string _buttonText;
+        public string ButtonText
+        {
+            get { return _buttonText; }
+            set { SetProperty(ref _buttonText, value); }
+        }
+
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        private string _locationEnteredByUser;
+        public string LocationEnteredByUser
+        {
+            get { return _locationEnteredByUser; }
+            set { SetProperty(ref _locationEnteredByUser, value); }
+        }
+
+        private ObservableCollection<WeatherItem> _weatherCollection = new ObservableCollection<WeatherItem>();
+        public ObservableCollection<WeatherItem> WeatherCollection
+        {
+            get { return _weatherCollection; }
+            set { SetProperty(ref _weatherCollection, value); }
+        }
+
+        INavigationService _navigationService;
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+
+            NavToNewPageCommand = new DelegateCommand(NavToNewPage);
+            GetWeatherForLocationCommand = new DelegateCommand(GetWeatherForLocation);
+            NavToMoreInfoPageCommand = new DelegateCommand<WeatherItem>(NavToMoreInfoPage);
+
+            Title = "Xamarin Forms Application + Prism";
+            ButtonText = "Add Name";
+        }
+
+        private async void NavToMoreInfoPage(WeatherItem weatherItem)
+        {
+            var navParams = new NavigationParameters();
+            navParams.Add("WeatherItemInfo", weatherItem);
+            await _navigationService.NavigateAsync("MoreInfoPage", navParams);
+        }
+
+        internal async void GetWeatherForLocation()
+        {
+            HttpClient client = new HttpClient();
+            var uri = new Uri(
+                string.Format(
+                    $"http://api.openweathermap.org/data/2.5/weather?q={LocationEnteredByUser}&units=imperial&APPID=" +
+                    $"{ApiKeys.WeatherKey}"));
+            var response = await client.GetAsync(uri);
+            WeatherItem weatherData = null;
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                weatherData = WeatherItem.FromJson(content);
+            }
+            WeatherCollection.Add(weatherData);
+        }
+
+        private async void NavToNewPage()
+        {
+            var navParams = new NavigationParameters();
+            navParams.Add("NavFromPage", "MainPageViewModel");
+            await _navigationService.NavigateAsync("SamplePageForNavigation", navParams);
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+        }
+    }
+}
+
---
+++ Week9PrismExampleApp/ViewModels/MoreInfoPageViewModel.cs
@@ -0,0 +1,49 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Prism.Navigation;
+using Xamarin.Forms.Xaml;
+using static Week9PrismExampleApp.Models.WeatherItemModel;
+
+namespace Week9PrismExampleApp.ViewModels
+{
+    public class MoreInfoPageViewModel : BindableBase, INavigatedAware
+    {
+		INavigationService _navigationService;
+
+		public DelegateCommand GoBackCommand { get; set; }
+
+        private WeatherItem _weatherItem;
+        public WeatherItem WeatherItem
+        {
+            get { return _weatherItem; }
+            set { SetProperty(ref _weatherItem, value); }
+        }
+
+        public MoreInfoPageViewModel(INavigationService navigationService)
+        {
+			_navigationService = navigationService;
+
+			GoBackCommand = new DelegateCommand(GoBack);
+		}
+
+		private void GoBack()
+		{
+			_navigationService.GoBackAsync();
+		}
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("WeatherItemInfo"))
+            {
+                WeatherItem = (WeatherItem)parameters["WeatherItemInfo"];
+            }
+        }
+    }
+}
